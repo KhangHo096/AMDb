bf7e8afc43ccf30b54e3c48d7ca954e16909e8e8
Thanh Quy <quythanh24@gmail.com>
2023-03-23 15:10:25 +0700

    test merge

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -2,7 +2,7 @@ part of 'profile.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
-
+  //
   @override
   Widget build(BuildContext context) {
     return BlocProvider<ProfileBloc>(
